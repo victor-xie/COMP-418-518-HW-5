ec1bd4bbede0ea9a8e02c1d1394f7d70332f45c8
Victor Xie <vyx2@rice.edu>
2025-04-16 17:32:30 -0500

    Finished GroupBy

--- homework5-code-released/src/ra/GroupBy.java
+++ homework5-code-released/src/ra/GroupBy.java
@@ -1,5 +1,10 @@
 package ra;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import dsl.Query;
 import dsl.Sink;
 import utils.Pair;
@@ -22,8 +27,22 @@ public class GroupBy<K,A,B> implements Query<Pair<K,A>,Pair<K,B>> {
 
 	// TODO
 
+	// Initial value
+	private final B init;
+
+	// Fold operator
+	private final Func2<B, A, B> op;
+
+	// Map of k -> b aggregations for each k
+	private final Map<K, B> aggs = new HashMap<>();
+	// FIFO order of k values seen as they come through the input channel
+	private final List<K> kFIFO = new ArrayList<>();
+	
+
 	private GroupBy(B init, Func2<B,A,B> op) {
 		// TODO
+		this.init = init;
+		this.op = op;
 	}
 
 	public static <K,A,B> GroupBy<K,A,B> from(B init, Func2<B,A,B> op) {
@@ -38,11 +57,32 @@ public class GroupBy<K,A,B> implements Query<Pair<K,A>,Pair<K,B>> {
 	@Override
 	public void next(Pair<K,A> item, Sink<Pair<K,B>> sink) {
 		// TODO
+		K k = item.getLeft();
+		A a = item.getRight();
+
+		// New k seen, add into the FIFO order
+		if (!aggs.containsKey(k)) {
+			// k is first mapped to the initial value
+			aggs.put(k, init);
+			kFIFO.add(k);
+		}
+		
+		// Aggregate a into the running b
+		B b = aggs.get(k);
+		B b_prime = op.apply(b, a);
+		aggs.put(k, b_prime);
 	}
 
 	@Override
 	public void end(Sink<Pair<K,B>> sink) {
 		// TODO
+		
+		// output k->b in FIFO order
+		for (K key : kFIFO) {
+			B b = aggs.get(key);
+			sink.next(Pair.from(key, b));
+		}
+		sink.end();
 	}
 	
 }
