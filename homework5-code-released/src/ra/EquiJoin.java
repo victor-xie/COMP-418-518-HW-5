43dad9c5dc00fe862b880b7181ffefa6db13d241
Victor Xie <vyx2@rice.edu>
2025-04-16 16:57:10 -0500

    EquiJoin and ThetaJoin pass

--- homework5-code-released/src/ra/EquiJoin.java
+++ homework5-code-released/src/ra/EquiJoin.java
@@ -1,5 +1,9 @@
 package ra;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.function.Function;
 
 import dsl.Query;
@@ -18,9 +22,20 @@ import utils.Pair;
 public class EquiJoin<A,B,T> implements Query<Or<A,B>,Pair<A,B>> {
 
 	// TODO
+	private final Function<A, T> f;
+	private final Function<B, T> g;
 
+
+	// Maps to provide O(1) access to all matching a or b values without function queries
+	// Stores all values a that produce f(a), i.e. f(a) -> [a_1,a_2,...]
+	private final Map<T, List<A>> aCache = new HashMap<>();
+	// Stores all values b that produce g(b), i.e. f(b) -> [b_1, b_2,...]
+	private final Map<T, List<B>> bCache = new HashMap<>();
+	
 	private EquiJoin(Function<A,T> f, Function<B,T> g) {
 		// TODO
+		this.f = f;
+		this.g = g;
 	}
 
 	public static <A,B,T> EquiJoin<A,B,T> from(Function<A,T> f, Function<B,T> g) {
@@ -35,11 +50,48 @@ public class EquiJoin<A,B,T> implements Query<Or<A,B>,Pair<A,B>> {
 	@Override
 	public void next(Or<A,B> item, Sink<Pair<A,B>> sink) {
 		// TODO
+
+		// a Value: Check for all b values that produce f(a) = g(b)
+		if (item.isLeft()) {
+			A a = item.getLeft();
+			// Get f(a)
+			T key = f.apply(a);
+			// Get all matching B values that produce g(b) (list is empty if there is no match)
+			List<B> matchingBs = bCache.getOrDefault(key, List.of());
+			// Each pair should be sent out
+			for (B b : matchingBs) {
+				sink.next(Pair.from(a, b));
+			}
+			
+			// Store the a value in its matching f(a) -> a cache
+			List<A> aList = aCache.get(key);
+			// New f(a) value
+			if (aList == null) {
+				aList = new ArrayList<>();
+				aCache.put(key, aList);
+			}
+			aList.add(a);
+		} else {
+			// Same process but for b
+			B b = item.getRight();
+			T key = g.apply(b);
+			List<A> aList = aCache.getOrDefault(key, List.of());
+			for (A a : aList) {
+				sink.next(Pair.from(a, b));
+			}
+			List<B> bList = bCache.get(key);
+			if (bList == null) {
+				bList = new ArrayList<>();
+				bCache.put(key, bList);
+			}
+			bList.add(b);
+		}
 	}
 
 	@Override
 	public void end(Sink<Pair<A,B>> sink) {
 		// TODO
+		sink.end();
 	}
 	
 }
--- homework5-code-released/src/ra/ThetaJoin.java
+++ homework5-code-released/src/ra/ThetaJoin.java
@@ -1,5 +1,7 @@
 package ra;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
 import java.util.function.BiPredicate;
 
 import dsl.Query;
@@ -18,9 +20,13 @@ import utils.Pair;
 public class ThetaJoin<A,B> implements Query<Or<A,B>,Pair<A,B>> {
 
 	// TODO
+	private final BiPredicate<A, B> theta;
+    private final Deque<A> leftVals = new ArrayDeque<>();
+    private final Deque<B> rightVals = new ArrayDeque<>();
 
 	private ThetaJoin(BiPredicate<A,B> theta) {
 		// TODO
+		this.theta = theta;
 	}
 
 	public static <A,B> ThetaJoin<A,B> from(BiPredicate<A,B> theta) {
@@ -35,11 +41,36 @@ public class ThetaJoin<A,B> implements Query<Or<A,B>,Pair<A,B>> {
 	@Override
 	public void next(Or<A,B> item, Sink<Pair<A,B>> sink) {
 		// TODO
+
+		// Check (a,b) for all currently seen b values
+		if (item.isLeft()) {
+            A a = item.getLeft();
+            for (B b : rightVals) {
+				// Apply theta
+                if (theta.test(a, b)) {
+					// Send the pair if the test passes
+                    sink.next(Pair.from(a, b));
+                }
+            }
+			// Store a
+            leftVals.addLast(a);
+        } else {
+			// Same process but check (a,b) for all currently seen a values
+            B b = item.getRight();
+            for (A a : leftVals) {
+                if (theta.test(a, b)) {
+                    sink.next(Pair.from(a, b));
+                }
+            }
+			// Store b
+            rightVals.addLast(b);
+        }
 	}
 
 	@Override
 	public void end(Sink<Pair<A,B>> sink) {
 		// TODO
+		sink.end();
 	}
 	
 }
