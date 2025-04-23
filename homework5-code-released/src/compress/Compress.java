dd1507304121e6892551237277d818c88096edbd
Victor Xie <84203383+victor-xie@users.noreply.github.com>
2025-04-22 19:59:53 -0500

    Update Compress.java
    manual save of draft

--- homework5-code-released/src/compress/Compress.java
+++ homework5-code-released/src/compress/Compress.java
@@ -1,6 +1,8 @@
 package compress;
 
 import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.Queue;
 
 import dsl.*;
 import ecg.Data;
@@ -9,47 +11,184 @@ public class Compress {
 
 	public static final int BLOCK_SIZE = 10;
 
-	public static Query<Integer,Integer> delta() {
+	// Will return the query for delta-encoding
+	public static Query<Integer, Integer> delta() {
 		// TODO
-		return null;
+		return new Query<>() {
+			int prev = 0;
+
+			@Override
+			public void start(Sink<Integer> sink) {
+				prev = 0;
+			}
+
+			@Override
+			public void next(Integer item, Sink<Integer> sink) {
+				int delta = item - prev;
+				sink.next(delta);
+				prev = item;
+			}
+
+			@Override
+			public void end(Sink<Integer> sink) {
+				sink.end();
+			}
+		};
 	}
 
-	public static Query<Integer,Integer> deltaInv() {
+	// Will return the query for delta-decoding (inverse)
+	public static Query<Integer, Integer> deltaInv() {
 		// TODO
-		return null;
+		return new Query<>() {
+			int prev = 0;
+
+			@Override
+			public void start(Sink<Integer> sink) {
+				prev = 0;
+			}
+
+			@Override
+			public void next(Integer item, Sink<Integer> sink) {
+				int value = item + prev;
+				sink.next(value);
+				prev = value;
+			}
+
+			@Override
+			public void end(Sink<Integer> sink) {
+				sink.end();
+			}
+		};
 	}
 
-	public static Query<Integer,Integer> zigzag() {
+	// Query for zigzag encoding
+	public static Query<Integer, Integer> zigzag() {
 		// TODO
-		return null;
+		return Q.map(n -> (n << 1) ^ (n >> 31));
 
 	}
 
-	public static Query<Integer,Integer> zigzagInv() {
+	// Query for inverse of zigzag decoding (inverse)
+	public static Query<Integer, Integer> zigzagInv() {
 		// TODO
-		return null;
+		return Q.map(z -> (z >>> 1) ^ -(z & 1));
 
 	}
 
-	public static Query<Integer,Integer> pack() {
+	// Query for packing blocks into byte-aligned compressed messages
+	public static Query<Integer, Integer> pack() {
 		// TODO
-		return null;
+		return new Query<>() {
+			Queue<Integer> block = new LinkedList<>();
+
+			@Override
+			public void start(Sink<Integer> sink) {
+				block.clear();
+			}
+
+			@Override
+			public void next(Integer item, Sink<Integer> sink) {
+				block.add(item);
+				if (block.size() == BLOCK_SIZE) {
+					int max = 0;
+					for (int val : block) max = Math.max(max, val);
+					int bits = 32 - Integer.numberOfLeadingZeros(max);
+					if (bits == 0) bits = 1;
+
+					sink.next(bits); // header: bit width
+
+					int acc = 0;
+					int accLen = 0;
+					for (int val : block) {
+						acc |= (val << accLen);
+						accLen += bits;
+						while (accLen >= 8) {
+							sink.next(acc & 0xFF);
+							acc >>>= 8;
+							accLen -= 8;
+						}
+					}
+					if (accLen > 0) {
+						sink.next(acc & 0xFF);
+					}
+					block.clear();
+				}
+			}
+
+			@Override
+			public void end(Sink<Integer> sink) {
+				sink.end();
+			}
+		};
 
 	}
 
-	public static Query<Integer,Integer> unpack() {
+	// Query for unpacking: compressed message -> recoverse elements that were
+	// packed
+	public static Query<Integer, Integer> unpack() {
 		// TODO
-		return null;
+		return new Query<>() {
+			private int bitWidth = 0;
+			private final LinkedList<Integer> buffer = new LinkedList<>();
+			private int acc = 0;
+			private int accLen = 0;
+			private int valuesDecoded = 0;
+
+			@Override
+			public void start(Sink<Integer> sink) {
+				bitWidth = 0;
+				acc = 0;
+				accLen = 0;
+				valuesDecoded = 0;
+				buffer.clear();
+			}
+
+			@Override
+			public void next(Integer item, Sink<Integer> sink) {
+				if (bitWidth == 0) {
+					bitWidth = item;
+					valuesDecoded = 0;
+					acc = 0;
+					accLen = 0;
+				} else {
+					acc |= (item << accLen);
+					accLen += 8;
+					while (accLen >= bitWidth && valuesDecoded < BLOCK_SIZE) {
+						int val = acc & ((1 << bitWidth) - 1);
+						sink.next(val);
+						acc >>>= bitWidth;
+						accLen -= bitWidth;
+						valuesDecoded++;
+					}
+					if (valuesDecoded == BLOCK_SIZE) {
+						bitWidth = 0;
+						acc = 0;
+						accLen = 0;
+					}
+				}
+			}
+
+			@Override
+			public void end(Sink<Integer> sink) {
+				sink.end();
+			}
+		};
 	}
 
-	public static Query<Integer,Integer> compress() {
+	// Query for compression
+	public static Query<Integer, Integer> compress() {
 		// TODO
-		return null;
+
+		// HINT: PIPELINE OF QUERIES
+		return Q.pipeline(delta(), zigzag(), pack());
 	}
 
-	public static Query<Integer,Integer> decompress() {
+	// Query for decompression
+	public static Query<Integer, Integer> decompress() {
 		// TODO
-		return null;
+
+		// HINT: PIPELINE OF QUERIES
+		return Q.pipeline(unpack(), zigzagInv(), deltaInv());
 	}
 
 	public static void main(String[] args) {
@@ -61,9 +200,9 @@ public class Compress {
 		System.out.println("***** Compress *****");
 		{
 			// from range [0,2048) to [0,256)
-			Query<Integer,Integer> q1 = Q.map(x -> x / 8);
-			Query<Integer,Integer> q2 = compress();
-			Query<Integer,Integer> q = Q.pipeline(q1, q2);
+			Query<Integer, Integer> q1 = Q.map(x -> x / 8);
+			Query<Integer, Integer> q2 = compress();
+			Query<Integer, Integer> q = Q.pipeline(q1, q2);
 			Iterator<Integer> it = Data.ecgStream("100-all.csv");
 			Q.execute(it, q, S.lastCount());
 		}
@@ -72,10 +211,10 @@ public class Compress {
 		System.out.println("***** Compress & Decompress *****");
 		{
 			// from range [0,2048) to [0,256)
-			Query<Integer,Integer> q1 = Q.map(x -> x / 8);
-			Query<Integer,Integer> q2 = compress();
-			Query<Integer,Integer> q3 = decompress();
-			Query<Integer,Integer> q = Q.pipeline(q1, q2, q3);
+			Query<Integer, Integer> q1 = Q.map(x -> x / 8);
+			Query<Integer, Integer> q2 = compress();
+			Query<Integer, Integer> q3 = decompress();
+			Query<Integer, Integer> q = Q.pipeline(q1, q2, q3);
 			Iterator<Integer> it = Data.ecgStream("100-all.csv");
 			Q.execute(it, q, S.lastCount());
 		}
